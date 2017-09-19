5d36f805e6061629e11a02ccc2e0cb1ca8f36487
Ing. Pavel Janousek <pjanouse@redhat.com>
2017-09-19 12:14:16 +0200

    .

--- src/test/resources/foreman_node_sharing_plugin/setup.sh
+++ src/test/resources/foreman_node_sharing_plugin/setup.sh
@@ -115,7 +115,7 @@ echo "** Updating host $HOSTNAME"
 curl -g -H "Content-Type: application/json" \
     -X PUT -d "$hostUpdateString" \
     -k -u $USER:$PASS \
-    $FOREMAN_URL/hosts/$HOSTNAME
+    $FOREMAN_URL/hosts/$HOSTNAME.$LOCALDOMAIN
 
 echo ""
 echo "** Done"
